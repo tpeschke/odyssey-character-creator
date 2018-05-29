193cf8e04fc6fa472a1f64020ebb3991f6e18976
Trent <mr.peschke@gmail.com>
2018-05-29 10:12:35 -0600

    finally got the db, graphql, and the server to play nice

--- db/seed_tables.sql
+++ db/seed_tables.sql
@@ -2,4 +2,13 @@ create table users (
     id serial Primary KEY,
     user_name VARCHAR(40),
     auth_id TEXT
-)
+);
+
+create table aliens (
+    id serial Primary KEY,
+    species VARCHAR(40),
+    selected Int DEFAULT 0
+);
+
+insert into aliens (species)
+values ('Akehlarian'), ('Clone'), ('Droid'), ('Ghost'), ('Human'), ('Hiven');
--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express')
+    , session = require('express-session')
     , bodyParser = require('body-parser')
     , cors = require('cors')
     , dotenv = require('dotenv').config()
     , massive = require('massive')
-    , session = require('express-session')
+    , gqlHTTP = require('express-graphql')
+    , { GraphQLServer } = require('graphql-yoga')
+    , graphql = require('graphql')
 
+    const { GraphQLObjectType : GObject, GraphQLString : GString, GraphQLSchema : GSchema, GraphQLID : GID, GraphQLInt : GInt, GraphQLList : GList, GraphQLNonNull: GNonNull } = graphql
     const {SERVER_PORT, SESSION_SECRET, CONNECTION_STRING} = process.env
 
     const app = new express()
@@ -47,12 +51,40 @@ app.get('/loginDummy', (req, res)=>res.send('hello'))
 // ===========AUTH END POINTS===========\\
 
 
+//============GRAPH QL SCHEMA============\\
+const AlienType = new GObject({
+    name: "Alien",
+    fields: ()=> ({
+        id: {type: GID},
+        species: {type: GString}
+    })
+})
 
-    
+const RootQuery = new GObject({
+    name: 'RootQueryType',
+    fields: {
+        aliens: {
+            type: new GList(AlienType),
+            resolve(parent, args) {
+                return db().aliens.find()
+            }
+        }
+    }
+}) 
+
+// ===========CONECTION THINGS============\\
+
+const db = function() {
+    return app.get('db')
+}
+
 massive(CONNECTION_STRING).then(dbInstance => {
     app.set('db', dbInstance)
-
-    app.listen(SERVER_PORT, _ => {
-        console.log(`Sing the song of your heart and read the mourning engraved there ${SERVER_PORT}`)
-    })
+    
+    app.use('/graphql', gqlHTTP({
+        schema: new GSchema({query: RootQuery}),
+        graphiql: true
+    }))
+    
+    app.listen(SERVER_PORT, _=> console.log(`Sing the song of your heart and read the mourning engraved there ${SERVER_PORT}`))
 })
